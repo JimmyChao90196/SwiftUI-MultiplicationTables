d948773cf6f0b15e538255355f026808befed3eb
Jimmy <myname90196@gmail.com>
2024-03-20 08:16:20 +0800

    Initial Commit

---
+++ SwiftUI-MultiplicationTables/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-MultiplicationTables
+//
+//  Created by JimmyChao on 2024/3/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-MultiplicationTables/SwiftUI_MultiplicationTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MultiplicationTablesApp.swift
+//  SwiftUI-MultiplicationTables
+//
+//  Created by JimmyChao on 2024/3/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MultiplicationTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
