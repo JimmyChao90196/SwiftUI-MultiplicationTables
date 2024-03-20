30126cb04c32193a802e6751463e4764fdb63271
Jimmy <myname90196@gmail.com>
2024-03-20 08:45:30 +0800

    -init- 1. initial commit

--- SwiftUI-MultiplicationTables/ContentView.swift
+++ SwiftUI-MultiplicationTables/ContentView.swift
@@ -6,8 +6,23 @@
 //
 
 import SwiftUI
+import Foundation
+
+struct Question {
+    
+    var baseNumber: Int
+    var randomNumber: Int
+    
+    var questions: String {
+        return "\(baseNumber) times \(randomNumber)"
+    }
+    var answer: Int {
+        return baseNumber * randomNumber
+    }
+}
 
 struct ContentView: View {
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -16,6 +31,18 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+    }
+    
+    func generateRandomNumbers(by num: Int) -> [Int]{
+        
+        var randNumbers = [Int]()
+        
+        for i in 0..<num {
+            let randNumber = Int.random(in: 0...12)
+            randNumbers.append(randNumber)
+        }
+        
+        return randNumbers
     }
 }
 
